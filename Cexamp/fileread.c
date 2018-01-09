ae493853cc8a111e0dd7b9a2b56cba600275dea0
srinivaskolaparthi <srinivas.kolaparthi@capgemini.com>
2018-01-09 12:44:29 +0530

    Add files via upload

---
+++ Cexamp/fileread.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char ch;
+    fp = fopen("one.txt", "w");
+    printf("Enter data...");
+    while( (ch = getchar()) != EOF) {
+        putc(ch, fp);
+    }
+    fclose(fp);
+    fp = fopen("one.txt", "r");
+ 
+    while( (ch = getc(fp)! = EOF)
+    printf("%c",ch);
+    
+    // closing the file pointer
+    fclose(fp);
+    
+    return 0;
+}
